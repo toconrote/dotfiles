83115f77d10e6a538dbb4f888b37041b93c33065
toconrote <example@exam.ple.com>
2015-12-09 16:01:47 +0900

    PERSONALUSE

--- MINE/just_for_strage/canvas.js
+++ MINE/just_for_strage/canvas.js
@@ -1,29 +1,38 @@
 $(function(){
   canvas = document.getElementById('c');
   ctx = canvas.getContext('2d');
-  pageX = 0;
-  pageY = 0;
-  $("#c").mousedown(function(eo){drawStart(eo.pageX, eo.pageY)});
-  $("body").mouseup(drawEnd);
+  ctx.lineCap = "round";
+  mx = 0;
+  my = 0;
+  $("body").mousedown(function(eo){if(eo.which=="1")drawStart()});
+  $("body").mousemove(function(eo){
+    mousepos(eo.pageX, eo.pageY, eo.which)
+  });
   $("#clear").click(clear);
 });
 
-function drawStart(x, y){
-  pageX = x;
-  pageY = y;
-  timer = setInterval(dot(), 66);
+function drawStart(){
+  ctx.beginPath();
+  ctx.moveTo(mx, my);
 }
-function dot(){
-  mousex = pageX - c.offsetLeft;
-  mousey = pageY - c.offsetTop;
-  ctx.rect(mousex,mousey,1,1);
-  ctx.fill();
+function draw() {
+  line(mx, my);
 }
-
-function drawEnd(){
-  clearInterval(timer);
+function mousepos(x, y, mb) {
+  mx = x - canvas.offsetLeft;
+  my = y - canvas.offsetTop;
+  $("#debug").html("mx:"+mx+" my:"+my);
+  if(mb == "1")line(mx, my);
+}
+function line(x, y){
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
 }
 
 function clear(){
-  ctx.clearRect(0,0,640,480);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0,0,640,480);
+  ctx.fillStyle = "black"
 }
