78a3db88e4470829d658f8b74f37815286621dc5
toconrote <example@exam.ple.com>
2015-12-28 14:39:36 +0900

    PERSONALUSE

--- MINE/just_for_strage/snake.js
+++ MINE/just_for_strage/snake.js
@@ -1,5 +1,8 @@
 MAX_X = 32;
 MAX_Y = 32;
+FPS = 60;
+SPEED = 15;
+chaos = true;
 
 $(function(){
   canvas = document.getElementById("c");
@@ -8,7 +11,9 @@ $(function(){
   canvas.width = MAX_X*10;
   canvas.height = MAX_Y*10;
 
+  esa = [];
   direction = 3;//下
+  len = 0; //体の長さ
   x = Math.floor(MAX_X / 2);
   y = Math.floor(MAX_Y / 2);
   timer = null;
@@ -22,16 +27,24 @@ $(function(){
   $("#start").click(function(){
     $(this).attr("disabled", "disabled");
     timer = setInterval(function(){
+      if(chaos){
+        deg = canvas.style.transform.match(/rotate\((.+)deg\)/)[1];
+        deg = deg == '360' ? 1 : Number(deg)+1;
+        canvas.style.transform = "rotate("+deg+"deg)"
+      }
       moveHead();
-      redraw();
-    }, 100);
+    }, Math.floor(1000/SPEED));
   });
+  setInterval(function(){
+    redraw();
+  }, Math.floor(1000/FPS));
 
   $("body").keydown(function(eo){
     getDirection(eo.which);
   });
 });
 
+//向きの変更
 function getDirection(dir) {
   if(dir==32){
     if(!timer){
@@ -39,10 +52,14 @@ function getDirection(dir) {
     }
     return;
   }
-  direction = dir - 37;
-  $("#debug").html(dirLetter[direction]);
+  if(dir==37 || dir==38 || dir==39 || dir==40) {
+    if(Math.abs(direction-(dir-37))==2)return;
+    direction = dir - 37;
+    $("#debug").html(dirLetter[direction]);
+  }
 }
 
+//移動計算
 function moveHead(){
   switch(direction) {
     case 0://←
@@ -68,8 +85,14 @@ function moveHead(){
     redraw();
     $("#start").removeAttr("disabled");
   }
+
+  //体更新
+  for(var i=0;i<len;i++) {
+
+  }
 }
 
+//canvas再描画
 function redraw(){
   ctx.fillStyle = "#FFFFFF";
   ctx.fillRect(0,0,MAX_X*10,MAX_Y*10);
